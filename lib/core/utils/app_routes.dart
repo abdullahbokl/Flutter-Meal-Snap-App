72ccfb74f327c923e7973160f51fb8807a663930
Bokl2002 <abdullah.bokl@gmail.com>
2023-12-22 08:02:37 +0200

    prepare routes

---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../../features/auth/presentation/screens/change_lang_screen.dart';
+import '../../features/auth/presentation/screens/login_screen.dart';
+import '../../features/auth/presentation/screens/reset_password.dart';
+import '../../features/auth/presentation/screens/send_code_screen.dart';
+import '../../features/auth/presentation/screens/splash_screen.dart';
+
+abstract class Routes {
+  static const String intitlRoute = '/';
+  static const String changeLang = '/changeLang';
+  static const String login = '/login';
+  static const String sendCode = '/sendCode';
+  static const String restPassword = '/restPassword';
+}
+
+abstract class AppRoutes {
+  static Route? generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.intitlRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      // Auth
+      case Routes.changeLang:
+        return MaterialPageRoute(builder: (_) => const ChangeLangScreen());
+      case Routes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case Routes.sendCode:
+        return MaterialPageRoute(builder: (_) => const SendCodeScreen());
+      case Routes.restPassword:
+        return MaterialPageRoute(builder: (_) => const ResetPasswordScreen());
+
+      default:
+        return MaterialPageRoute(
+            builder: (_) => const Scaffold(
+                  body: Center(
+                    child: Text('No Found Route'),
+                  ),
+                ));
+    }
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'core/utils/app_routes.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, World!');
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.intitlRoute,
+      onGenerateRoute: AppRoutes.generateRoute,
+      title: 'Meal Snap',
+    );
   }
 }
