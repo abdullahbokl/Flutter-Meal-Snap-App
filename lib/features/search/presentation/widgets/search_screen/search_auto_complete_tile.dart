239fdd88cec53ec466b92ebc23b738a49c953719
Bokl2002 <abdullah.bokl@gmail.com>
2023-12-31 14:48:30 +0200

    SearchResultsListView

--- lib/features/search/presentation/widgets/search_screen/search_auto_complete_tile.dart
+++ lib/features/search/presentation/widgets/search_screen/search_auto_complete_tile.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:meal_snap/core/common/widgets/custom_text_widget.dart';
 import 'package:meal_snap/core/utils/app_navigator.dart';
 import 'package:meal_snap/core/utils/app_routes.dart';
+import 'package:meal_snap/core/utils/app_styles.dart';
 import 'package:meal_snap/features/recipe_info/data/models/recipe_info_screen_arguments.dart';
 
 import '../../../data/models/search_auto_complete_model.dart';
@@ -17,43 +19,40 @@ class SearchAutoCompleteTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        decoration: BoxDecoration(boxShadow: const [
-          BoxShadow(
-            offset: Offset(-2, -2),
-            blurRadius: 12,
-            color: Color.fromRGBO(0, 0, 0, 0.05),
-          ),
-          BoxShadow(
-            offset: Offset(2, 2),
-            blurRadius: 5,
-            color: Color.fromRGBO(0, 0, 0, 0.10),
-          )
-        ], borderRadius: BorderRadius.circular(10), color: Colors.white),
-        child: ListTile(
-          onTap: () {
-            AppNavigator.pushNamed(
-              Routes.recipeInfoScreen,
-              arguments: RecipeInfoScreenArguments(id: item.id),
-            );
-          },
-          leading: Container(
-            width: 100,
-            height: 60,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey,
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: CachedNetworkImageProvider(item.image))),
-          ),
-          title: Text(
-            item.name,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            decoration: AppStyles.primaryBoxDecoration,
+            child: ListTile(
+              onTap: () {
+                AppNavigator.pushNamed(
+                  Routes.recipeInfoScreen,
+                  arguments: RecipeInfoScreenArguments(id: item.id),
+                );
+              },
+              leading: Container(
+                  width: constraints.maxWidth * 0.3,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: CachedNetworkImageProvider(item.image),
+                    ),
+                  )),
+              title: CustomTextWidget(
+                text: item.name,
+                style: AppStyles.font16SatoshiBold.copyWith(
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
