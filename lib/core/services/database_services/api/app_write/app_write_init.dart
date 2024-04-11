21aae37819a6462440b921d6ee81e0533c6f7cc9
Eng. Abdullah Elbokl <abdullah.bokl@gmail.com>
2024-04-11 02:30:57 +0200

    update appwrited auth

--- lib/core/services/database_services/api/app_write/app_write_init.dart
+++ lib/core/services/database_services/api/app_write/app_write_init.dart
@@ -6,6 +6,7 @@ import '../../../../utils/app_strings.dart';
 Client appWriteInit() {
   final Client client = Client();
 
+  // AppwriteException: general_unknown_origin, Invalid Origin. Register your new client (com.bokl.meal_snap) as a new Android platform on your project console dashboard (403)
   client
     ..setEndpoint(AppStrings.appWriteEndPoint)
     ..setProject(dotenv.env[AppStrings.envAppWriteProjectId]);
